2d050e98b56447e473b7fd1a87750e3ec7e5907a
xingzai <35978718+447166939@users.noreply.github.com>
2026-03-03 00:22:43 +0800

    添加分页查询员工

---
+++ junhong_pojo/src/main/java/com/junhong/dto/EmployeePageQueryDTO.java
@@ -0,0 +1,19 @@
+package com.junhong.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class EmployeePageQueryDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+    //员工姓名
+    private String name;
+
+    //页码
+    private int page;
+
+    //每页显示记录数
+    private int pageSize;
+
+}
